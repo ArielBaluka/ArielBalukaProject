de018c9976b7ee7736469b871701c7cc1a657cb1
ariel baluka <arielbaluka@gmail.com>
2023-10-30 16:19:08 +0200

    Add project files.

---
+++ Model/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class User: BaseEntity
+    {
+        protected string firstname;
+        protected string lastname;
+        protected string userName;
+        protected string password;
+        protected DateTime birthDate;
+        protected bool gender;
+        protected string Email;
+        public string FirstName { get { return this.firstname; } set { this.firstname = value; } }
+        public string LastName { get { return this.lastname; } set { this.lastname = value; } }
+        public string UserName { get { return this.userName; } set { this.userName = value; } }
+        public string PassWord { get { return this.password; } set { this.password = value; } }
+        public DateTime BIRTHDATE { get { return this.birthDate; } set { this.birthDate = value; } }
+        public bool Gender { get { return this.gender; } set { this.gender = value; } }
+        public string EMAIL { get { return this.Email; } set { this.Email = value; } }
+
+    }
+    public class UserList : List<User>
+    {
+        //בנאי ברירת מחדל - אוסף ריק
+        public UserList() { }
+        //המרה אוסף גנרי לרשימת משתמשים
+        public UserList(IEnumerable<User> list)
+            : base(list) { }
+        //המרה מטה מטיפוס בסיס לרשימת משתמשים
+        public UserList(IEnumerable<BaseEntity> list)
+            : base(list.Cast<User>().ToList()) { }
+    }
+}
---
+++ ViewModel/UserDB.cs
@@ -0,0 +1,62 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class UserDB : BaseDB
+    {
+        protected override BaseEntity NewEntity()
+        {
+            return new User();
+        }
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            User user = entity as User;
+            user.ID = int.Parse(reader["id"].ToString());
+            user.FirstName = reader["firstname"].ToString(); 
+            user.LastName = reader["lastname"].ToString(); 
+            user.UserName = reader["userName"].ToString();
+            user.PassWord = reader["Password"].ToString();
+            user.Gender = reader["gender"].Equals(true); // true -> male
+            user.BIRTHDATE = DateTime.Parse(reader["birthDate"].ToString());
+            user.EMAIL = reader["Email"].ToString();
+
+            return user;
+        }
+
+        //שאילתה המחזירה את כלל המשתמשים
+        public UserList SelectAll()
+        {
+            command.CommandText = "SELECT * FROM tblUser";
+            UserList users = new UserList(ExecuteCommand());
+            return users;
+        }
+
+        //שאילתה המחזירה משתמש לפי מאפיין (id)
+        public User SelectByID(int id)
+        {
+            command.CommandText = "SELECT * FROM tblUser WHERE id=" + id.ToString() + ";";
+            UserList users = new UserList(ExecuteCommand());
+            if(users.Count() > 0)
+            {
+                return users[0];
+            }
+            return null;
+        }
+
+        public bool Login(User user)
+        {
+            command.CommandText = "SELECT * FROM tblUser Where userName ='"+ user.UserName + "';";
+            UserList users = new UserList(ExecuteCommand());
+            if(users.Count() > 0 && users[0].PassWord.Equals(user.PassWord))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
